422ffa09633dfd53ac2e3896f58c2fa71649b61d
Mitsutaka Takada <mtakada@nces.i.nagoya-u.ac.jp>
2025-10-04 18:34:32 +0900

    HAKONIWA_VERSIONをDockerfileおよび関連スクリプトに追加し、バージョンをv1.2.0に更新

--- hako/docker/create-image.ps1
+++ hako/docker/create-image.ps1
@@ -7,4 +7,4 @@ $IMAGE_TAG = Get-Content appendix/latest_version.txt
 $DOCKER_IMAGE = $IMAGE_NAME+':'+$IMAGE_TAG
 $DOCKER_FILE = 'docker/Dockerfile'
 
-docker build -f $DOCKER_FILE -t $DOCKER_IMAGE .
+docker build -f $DOCKER_FILE -t $DOCKER_IMAGE --build-arg HAKONIWA_VERSION=$IMAGE_TAG .
